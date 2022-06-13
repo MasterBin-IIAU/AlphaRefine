c3c52d3511a4a30895d63c63aa64eee1001607f0
Alexander Ivkin <a@ivkin.dev>
2022-06-13 11:59:11 -0400

    Removed extra code

--- run_dimp.py
+++ run_dimp.py
@@ -1,12 +1,8 @@
 import argparse
-import distutils
 import os
 import glob
 import torch
 import numpy as np
-import sys
-from pytracking.refine_modules.refine_module import RefineModule
-from pytracking.RF_utils import bbox_clip
 import time
 from pathlib import Path
 import math
@@ -59,201 +55,6 @@ def delta2bbox(delta):
     bbox_xywh[:, :2] = bbox_cxcywh[:, :2] - 0.5 * bbox_cxcywh[:, 2:]
     return bbox_xywh
 
-
-####
-
-# pytracking/refine_modules/refine_module
-class RefineModule(object):
-    def __init__(self, refine_net_dir, selector=None, search_factor=2.0, input_sz=256):
-        # print('refine_net_dir = ' + refine_net_dir)
-        self.refine_network = self.get_network(refine_net_dir)
-        assert isinstance(selector, (int, str))
-        self.branch_selector = selector if isinstance(selector, int) else self.get_network(selector)
-        self.search_factor = search_factor
-        self.input_sz = input_sz
-        self.mean = np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3))
-        self.std = np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3))
-
-    def initialize(self, frame1, bbox1):
-        """
-        Args:
-            frame1(np.array): cv2 iamge array with shape (H,W,3)
-            bbox1(np.array): with shape(4,)
-        """
-
-        """Step1: get cropped patch(tensor)"""
-        patch1, h_f, w_f = sample_target_SE(frame1, bbox1, self.search_factor, self.input_sz, mode=cv2.BORDER_CONSTANT)
-        patch1_tensor = self.img_preprocess(patch1)
-
-        """Step2: get GT's cooridinate on the cropped patch(tensor)"""
-        crop_sz = torch.Tensor((self.input_sz, self.input_sz))
-        bbox1_tensor = self.gt_preprocess(bbox1)  # (4,)
-        bbox1_crop_tensor = transform_image_to_crop_SE(bbox1_tensor, bbox1_tensor, h_f, w_f, crop_sz).cuda()
-
-        """Step3: forward prop (reference branch)"""
-        with torch.no_grad():
-            self.refine_network.forward_ref(patch1_tensor, bbox1_crop_tensor)
-
-    def refine(self, Cframe, Cbbox, mode='all', test=False):
-        """
-        Args:
-            Cframe: Current frame(cv2 array)
-            Cbbox: Current bbox (ndarray) (x1,y1,w,h)
-        """
-        tic = time.time()
-        if mode not in ['bbox', 'mask', 'corner', 'all']:
-            raise ValueError("mode should be 'bbox' or 'mask' or 'corner' or 'all' ")
-
-        """ Step1: get cropped patch (search region) """
-        Cpatch, h_f, w_f = sample_target_SE(Cframe, Cbbox, self.search_factor, self.input_sz, mode=cv2.BORDER_CONSTANT)
-        Cpatch_tensor = self.img_preprocess(Cpatch)
-
-        """ Step2: forward prop (test branch) """
-        output_dict = {}
-        with torch.no_grad():
-
-            output = self.refine_network.forward_test(Cpatch_tensor, mode='test')  # (1,1,H,W)
-
-            if mode == 'bbox' or mode == 'corner':
-                Pbbox_arr = self.pred2bbox(output, input_type=mode)
-                output_dict[mode] = self.bbox_back(Pbbox_arr, Cbbox, h_f, w_f)
-
-            elif mode == 'mask':
-                Pmask_arr = self.pred2bbox(output, input_type=mode)
-                output_dict['mask'] = map_mask_back(Cframe, Cbbox, self.search_factor, Pmask_arr,
-                                                    mode=cv2.BORDER_CONSTANT)
-
-            else:
-                boxes = []
-                box = [0, 0, 0, 0]
-                if 'bbox' in output:
-                    Pbbox_arr = self.pred2bbox(output, input_type='bbox')
-                    output_dict['bbox'] = self.bbox_back(Pbbox_arr, Cbbox, h_f, w_f)
-                    boxes.append(output_dict['bbox'])
-                    box = output_dict['bbox']  # for mask absense
-
-                if 'corner' in output:
-                    Pbbox_arr = self.pred2bbox(output, input_type='corner')
-                    output_dict['corner'] = self.bbox_back(Pbbox_arr, Cbbox, h_f, w_f)
-                    boxes.append(output_dict['corner'])
-                    box = output_dict['corner']
-
-                if 'mask' in output:
-                    Pmask_arr = self.pred2bbox(output, input_type='mask')
-                    output_dict['mask'] = map_mask_back(Cframe, Cbbox, self.search_factor, Pmask_arr,
-                                                        mode=cv2.BORDER_CONSTANT)
-                    output_dict['mask_bbox'] = mask2bbox(output_dict['mask'], box)
-                    boxes.append(output_dict['mask_bbox'])
-
-                if not isinstance(self.branch_selector, int):
-                    branch_scores = self.branch_selector(output['feat'])
-                    _, max_idx = torch.max(branch_scores.squeeze(), dim=0)
-                    max_idx = max_idx.item()
-                else:
-                    max_idx = self.branch_selector
-                output_dict['all'] = boxes[max_idx]
-        # print(time.time() - tic)
-        return output_dict if test else output_dict[mode]
-
-    def get_mask(self, Cframe, Cbbox, mode='all', test=False):
-        """
-        Args:
-            Cframe: Current frame(cv2 array)
-            Cbbox: Current bbox (ndarray) (x1,y1,w,h)
-        """
-        if mode not in ['bbox', 'mask', 'corner', 'all']:
-            raise ValueError("mode should be 'bbox' or 'mask' or 'corner' or 'all' ")
-
-        """ Step1: get cropped patch (search region) """
-        Cpatch, h_f, w_f = sample_target_SE(Cframe, Cbbox, self.search_factor, self.input_sz, mode=cv2.BORDER_CONSTANT)
-        Cpatch_tensor = self.img_preprocess(Cpatch)
-
-        """ Step2: forward prop (test branch) """
-        output_dict = {}
-        with torch.no_grad():
-            output = self.refine_network.forward_test(Cpatch_tensor, mode='test', branches=['mask'])  # (1,1,H,W)
-
-            assert 'mask' in output
-            Pmask_arr = self.pred2bbox(output, input_type='mask')
-            output_dict['mask'] = map_mask_back(Cframe, Cbbox, self.search_factor, Pmask_arr,
-                                                mode=cv2.BORDER_CONSTANT)
-
-        return output_dict['mask']
-
-    def pred2bbox(self, prediction, input_type=None):
-        if input_type == 'bbox':
-            Pbbox = prediction['bbox']
-            Pbbox = delta2bbox(Pbbox)
-            Pbbox_arr = np.array(Pbbox.squeeze().cpu())
-            return Pbbox_arr
-
-        elif input_type == 'corner':
-            Pcorner = prediction['corner']  # (x1,y1,x2,y2)
-            Pbbox_arr = np.array(Pcorner.squeeze().cpu())
-            Pbbox_arr[2:] = Pbbox_arr[2:] - Pbbox_arr[:2]  # (x1,y1,w,h)
-            return Pbbox_arr
-
-        elif input_type == 'mask':
-            Pmask = prediction['mask']
-            Pmask_arr = np.array(Pmask.squeeze().cpu())  # (H,W) (0,1)
-            return Pmask_arr
-
-        else:
-            raise ValueError("input_type should be 'bbox' or 'mask' or 'corner' ")
-
-    def bbox_back(self, bbox_crop, bbox_ori, h_f, w_f):
-        """
-        Args:
-            bbox_crop: coordinate on (256x256) region in format (x1,y1,w,h) (4,)
-            bbox_ori: origin traking result (x1,y1,w,h) (4,)
-            h_f: h scale factor
-            w_f: w scale factor
-        Return:
-            coordinate mapping back to origin image
-        """
-        x1_c, y1_c, w_c, h_c = bbox_crop.tolist()
-        x1_o, y1_o, w_o, h_o = bbox_ori.tolist()
-        x1_oo = x1_o - (self.search_factor - 1) / 2 * w_o
-        y1_oo = y1_o - (self.search_factor - 1) / 2 * h_o
-        delta_x1 = x1_c / w_f
-        delta_y1 = y1_c / h_f
-        delta_w = w_c / w_f
-        delta_h = h_c / h_f
-        return np.array([x1_oo + delta_x1, y1_oo + delta_y1,
-                         delta_w, delta_h])
-
-    def get_network(self, checkpoint_dir):
-        # print('refine network load')
-        # print('checkpoint_dir = ' + checkpoint_dir)
-        network, _ = load_network(checkpoint_dir)
-        network.cuda()
-        network.eval()
-        return network
-
-    def img_preprocess(self, img_arr):
-        """ to torch.Tensor(RGB), normalized (minus mean, divided by std)
-        Args:
-            img_arr: (H,W,3)
-        Return:
-            (1,1,3,H,W)
-        """
-        norm_img = ((img_arr / 255.0) - self.mean) / (self.std)
-        img_f32 = norm_img.astype(np.float32)
-        img_tensor = torch.from_numpy(img_f32).cuda()
-        img_tensor = img_tensor.permute((2, 0, 1))
-        return img_tensor.unsqueeze(dim=0).unsqueeze(dim=0)
-
-    def gt_preprocess(self, gt_arr):
-        """
-        Args:
-            gt_arr: ndarray (4,)
-        Return:
-            `torch.Tensor` (4,)
-        """
-        return torch.from_numpy(gt_arr.astype(np.float32))
-
-
-####
 
 # ltr/data/processing_utils_SE
 def sample_target_SE(im, target_bb, search_area_factor, output_sz=None, mode=cv2.BORDER_REPLICATE):
@@ -1997,8 +1798,6 @@ class DBLoader(object):
         self.im_paths.sort()
         self.init_box = self.get_init_box()
 
-
-
     def get_gt_box(self):
         init_box = np.array(np.loadtxt(self.gt_file, delimiter=',', dtype=np.float64))
         return init_box
@@ -2047,6 +1846,7 @@ def get_axis_aligned_bbox(region):
         cy = y + h / 2
     return cx, cy, w, h
 
+
 def get_init_box(im_dir):
     path = os.path.join(im_dir, "groundtruth.txt")
     ground_truth_rect = np.loadtxt(path, delimiter=',', dtype=np.float64)
@@ -2054,11 +1854,13 @@ def get_init_box(im_dir):
         return ground_truth_rect[0]
     return ground_truth_rect
 
+
 def get_new_frame(frame_id, im_dir):
     im_paths = glob.glob(os.path.join(im_dir, '*.jpg'))
     if len(im_paths) <= frame_id:
         return None
     return cv2.cvtColor(cv2.imread(im_paths[frame_id]), cv2.COLOR_BGR2RGB)
+
 
 def get_dimp(img, init_box, model_path):
     """ set up DiMPsuper as the base tracker """
@@ -2066,7 +1868,7 @@ def get_dimp(img, init_box, model_path):
     params.debug = 0
     params.visualization = False
 
-    params.use_gpu = True
+    params.use_gpu = False
 
     params.image_sample_size = 22 * 16
     params.search_area_scale = 6
@@ -2125,8 +1927,6 @@ def get_dimp(img, init_box, model_path):
                                  use_gpu=params.use_gpu)
 
     params.vot_anno_conversion_type = 'preserve_area'
-    params.visualization = True
-    params.debug = False
     params.visdom_info = {'use_visdom': False, 'server': '127.0.0.1', 'port': 8097}
     tracker = DiMP(params)
 
@@ -2143,22 +1943,13 @@ def get_dimp(img, init_box, model_path):
     return tracker
 
 
-def get_ar(img, init_box, ar_path):
-    """ set up Alpha-Refine """
-    selector_path = 0
-    sr = 2.0
-    input_sz = int(128 * sr)  # 2.0 by default
-    RF_module = RefineModule(ar_path, selector_path, search_factor=sr, input_sz=input_sz)
-    RF_module.initialize(img, np.array(init_box))
-    return RF_module
-
-
 def save_bb(data_dir, res):
     file = open("tracking res/" + os.path.split(data_dir)[1] + ".txt", "a")
     res = np.rint(np.array(res))
     res = res.reshape(1, res.shape[0])
     np.savetxt(file, res, fmt='%.2f', delimiter=',')
     file.close()
+
 
 def vis_video(data_dir, image, frame_id, res):
     color = (0, 0, 255)
@@ -2169,7 +1960,8 @@ def vis_video(data_dir, image, frame_id, res):
     cv2.rectangle(image, (s_x, s_y), (e_x, e_y), color, 2)
     cv2.imwrite(data_dir + "\\frame%d.jpg" % frame_id, image)
 
-def track_images(get_init_box, get_new_frame, model_path, data_path, save_res, use_ar=True):
+
+def track_images(get_init_box, get_new_frame, model_path, data_path, save_res):
     init_box = get_init_box(data_path)
     img = get_new_frame(0, data_path)
     H, W, _ = img.shape
@@ -2177,9 +1969,6 @@ def track_images(get_init_box, get_new_frame, model_path, data_path, save_res, u
     """ Step 1: set up base tracker and Alpha-Refine """
     dimp_path = os.path.join(model_path, 'new_DiMPnet_ep0005.pth.tar')
     tracker = get_dimp(img, init_box, dimp_path)
-    if use_ar:
-        ar_path = os.path.join(model_path, 'SEcmnet_ep0040-c.pth.tar')
-        RF_module = get_ar(img, init_box, ar_path)
     file = open("tracking res/" + os.path.split(data_path)[1] + ".txt", "w")
     save_res(data_path, init_box)
     ts = []
@@ -2195,10 +1984,6 @@ def track_images(get_init_box, get_new_frame, model_path, data_path, save_res, u
         # track with base tracker
         outputs = tracker.track(img)
         pred_bbox = outputs['target_bbox']
-
-        if use_ar:
-            """ Step 3: refine tracking results with Alpha-Refine """
-            pred_bbox = RF_module.refine(img, np.array(pred_bbox))
 
         """ Step 4: update base tracker's state with refined result """
         # x1, y1, w, h = pred_bbox.tolist()
@@ -2223,7 +2008,7 @@ def track_images(get_init_box, get_new_frame, model_path, data_path, save_res, u
     print("FPS = {}".format(fps))
 
 
-def track_video(get_init_box, model_path, video_path, save_res, use_ar=True):
+def track_video(get_init_box, model_path, video_path, save_res):
     vidcap = cv2.VideoCapture(video_path)
     vis_dir = os.path.join(os.path.split(video_path)[0], "vis_res")
     if not os.path.exists(vis_dir):
@@ -2235,9 +2020,6 @@ def track_video(get_init_box, model_path, video_path, save_res, use_ar=True):
     """ Step 1: set up base tracker and Alpha-Refine """
     dimp_path = os.path.join(model_path, 'new_DiMPnet_ep0005.pth.tar')
     tracker = get_dimp(image, init_box, dimp_path)
-    if use_ar:
-        ar_path = os.path.join(model_path, 'SEcmnet_ep0040-c.pth.tar')
-        RF_module = get_ar(image, init_box, ar_path)
     save_res(vis_dir, image, 1, init_box)
     ts = []
     frame_id = 2
@@ -2251,10 +2033,6 @@ def track_video(get_init_box, model_path, video_path, save_res, use_ar=True):
         # track with base tracker
         outputs = tracker.track(image)
         pred_bbox = outputs['target_bbox']
-
-        if use_ar:
-            """ Step 3: refine tracking results with Alpha-Refine """
-            pred_bbox = RF_module.refine(image, np.array(pred_bbox))
 
         """ Step 4: update base tracker's state with refined result """
         # x1, y1, w, h = pred_bbox.tolist()
@@ -2276,15 +2054,14 @@ def track_video(get_init_box, model_path, video_path, save_res, use_ar=True):
     fps = len(ts) / sum(t for t in ts)
     print("FPS = {}".format(fps))
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run tracker on sequence or dataset.')
     parser.add_argument('model_path', type=str, default=None, help='Path to the directory with models')
     parser.add_argument('dataset_path', type=str, default=None, help='Path to the dataset')
-    parser.add_argument('use_refine', type=str, default=None, help='Use Refine module')
     args = parser.parse_args()
-    use_ar = args.use_refine.lower() in ("yes", "true", "t", "1")
 
     if os.path.isfile(args.dataset_path):
-        track_video(get_init_box, args.model_path, args.dataset_path, vis_video, use_ar)
+        track_video(get_init_box, args.model_path, args.dataset_path, vis_video)
     else:
-        track_images(get_init_box, get_new_frame, args.model_path, args.dataset_path, save_bb, use_ar)
+        track_images(get_init_box, get_new_frame, args.model_path, args.dataset_path, save_bb)
